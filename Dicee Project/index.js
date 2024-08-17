9a10de763c06770eadc2b33f5966310669a57cdc
vinayramteke <22bee120@nith.ac.in>
2024-08-17 10:35:30 +0530

    Update dice images and add random number generation for dice roll

--- Dicee Project/index.js
+++ Dicee Project/index.js
@@ -1,1 +1,4 @@
 var randomNumber1=Math.floor(Math.random()*6)+1;
+document.querySelector(".img1").setAttribute("src","images/dice"+randomNumber1+".png");
+var randomNumber2=Math.floor(Math.random()*6)+1;
+document.querySelector(".img2").setAttribute("src","images/dice"+randomNumber2+".png");
