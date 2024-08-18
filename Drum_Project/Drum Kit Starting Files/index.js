da0ead375620d951e52ea60ff468c5d127d06791
vinayramteke <22bee120@nith.ac.in>
2024-08-18 18:28:38 +0530

    Add audio functionality for button clicks

--- Drum_Project/Drum Kit Starting Files/index.js
+++ Drum_Project/Drum Kit Starting Files/index.js
@@ -3,6 +3,7 @@ var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 // Detecting Button Press
 for (var i = 0; i < numberOfDrumButtons; i++) {
   document.querySelectorAll("button")[i].addEventListener("click", function () {
-    alert("I got clicked!");
+   var audio = new Audio("sounds/tom-1.mp3");
+    audio.play();
   });
 }
