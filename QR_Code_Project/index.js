32782fe922cb64333de9228a63d22cb732c1550d
vinayramteke <22bee120@nith.ac.in>
2024-08-25 00:41:02 +0530

    Add URL.txt file with npm package link

--- QR_Code_Project/index.js
+++ QR_Code_Project/index.js
@@ -19,6 +19,11 @@ inquirer
       var qr_svg = qr.image(url);
       qr_svg.pipe(fs.createWriteStream('qr_img.png'));
 
+      fs.writeFile('URL.txt', url, (err) => {
+        if (err) throw err;
+        console.log('The file has been saved!');
+      } );
+
     })
     .catch((error) => {
         if (error.isTtyError) {
