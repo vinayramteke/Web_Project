d2ad567d01e2b8150201904666c688b6217bb38b
vinayramteke <22bee120@nith.ac.in>
2024-09-01 16:48:44 +0530

    feat: Add dynamic year to footer

--- Band-Generator-Project/index.js
+++ Band-Generator-Project/index.js
@@ -12,7 +12,8 @@ app.use(express.static("public"));
 
 //Step 4 - Add a dynamic year to the footer.
 //Hint: Google to find out how to get the current year using JS.
-
+const currentYear = new Date().getFullYear();
+console.log(currentYear);
  
 
 app.use(bodyParser.urlencoded({ extended: true }));
